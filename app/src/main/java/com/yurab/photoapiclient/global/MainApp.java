5d63c2fa71d61bf9e16098fdfec68073cb2d34fb
yura.breza@gmail.com <Frombolivia91>
2017-10-02 11:53:06 +0300

    removed comment

--- app/src/main/java/com/yurab/photoapiclient/global/MainApp.java
+++ app/src/main/java/com/yurab/photoapiclient/global/MainApp.java
@@ -6,8 +6,6 @@ import android.content.Context;
 
 public class MainApp extends Application {
 
-    // i know it would be better not to create static context references
-    //and use dagger 2 instead...
     private static MainApp mInstance;
 
     @Override
